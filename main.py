d5bd682cccf844654c755d2318c0a8071ac235fa
TheArtistGuy <info@kaimueller-art.de>
2021-09-22 18:02:06 +0200

    some refactoring

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import my_projects.gauss_io
+import my_projects.picture_difference_io
+import my_projects.gauss_one_picture_io
+import my_projects.echo_io
+import my_projects.echo_alpha_io
 
 if __name__ == '__main__':
-    my_projects.gauss_io.run()
+    my_projects.picture_difference_io.run()
+
